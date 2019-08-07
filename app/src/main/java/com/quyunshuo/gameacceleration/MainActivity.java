87e10273024ad9551fb5cea4d4745df4a186fe2e
屈云硕 <quyunshuo@qq.com>
2019-08-07 18:16:12 +0800

    创建项目

---
+++ app/src/main/java/com/quyunshuo/gameacceleration/MainActivity.java
@@ -0,0 +1,19 @@
+package com.quyunshuo.gameacceleration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
